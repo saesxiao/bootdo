84f932b35049d0d8599a87dc6cc741c429a8469a
萧畇宇 <615131288@qq.com>
2019-04-14 18:33:22 +0800

    代理订单

--- src/main/java/com/bootdo/goodsManager/controller/GmOrderController.java
+++ src/main/java/com/bootdo/goodsManager/controller/GmOrderController.java
@@ -117,7 +117,7 @@ public class GmOrderController {
 		if(user==null){
 			ShiroUtils.logout();
 		}
-		Map<String,List<GmOrderDO>> res = new LinkedHashMap<>();
+		Map<String,Map<String,Object>> res = new LinkedHashMap<>();
 		Long userId = user.getUserId();
 		Map<String,Object> query = new HashMap<>();
 		query.put("parentId",userId);
@@ -125,11 +125,12 @@ public class GmOrderController {
 		query = new HashMap<>();
 		query.put("userId",userId);
 		orderList.addAll(gmOrderService.list(query));
+
 		for (GmOrderDO order:orderList) {
 			GmGoodsInfoDO goodsInfo = goodsInfoService.get(order.getGoodsId());
 			order.setGoodsName(goodsInfo.getGoodsName());
 			String type = "";
-			UserDO userDO = userService.getById(order.getUserId());
+			String niceName = userService.getById(order.getUserId()).getName();
 			if(order.getUserId()==userId&&order.getParentId()!=userId){
 				type = "1";
 			}else if(order.getParentId()==userId&&order.getUserId()!=userId){
@@ -137,15 +138,26 @@ public class GmOrderController {
 			}else if(order.getParentId()==userId&&order.getUserId()!=userId&&order.getOrderStatus().equals("2")){
 				type = "3";
 			}
-			String orderCode = order.getType()+"-"+type+"-"+userDO.getName();
+			Double money = goodsInfo.getGoodsPrice()*order.getGoodsNum();
+			String orderCode = order.getType(); //+"-"+type+"-"+userDO.getName()+"-"+money
 			if(res.containsKey(orderCode)){
-				List<GmOrderDO> tempList = res.get(orderCode);
+				Map<String,Object> map= res.get(orderCode);
+				List<GmOrderDO> tempList = (List<GmOrderDO>) map.get("data");
 				tempList.add(order);
-				res.replace(orderCode,tempList);
+				map.replace("data",tempList);
+				map.put("type",type);
+				map.put("name",niceName);
+				map.put("money",money);
+				res.replace(orderCode,map);
 			}else{
 				List<GmOrderDO> tempList = new ArrayList<>();
 				tempList.add(order);
-				res.put(orderCode,tempList);
+				Map<String,Object> map= new HashMap<>();
+				map.put("data",tempList);
+				map.put("type",type);
+				map.put("name",niceName);
+				map.put("money",money);
+				res.put(orderCode,map);
 			}
 		}
 		return res;
@@ -187,145 +199,147 @@ public class GmOrderController {
 			if(orderList==null||orderList.size()<1){
 				return R.error("订单无效");
 			}
-			// 获取订单
-			GmOrderDO order = orderList.get(0);
-			if(order.getOrderStatus()!=1){
-				return R.error("已处理或失效订单");
-			}
-			// 通过当前账户id 和 订单的商品id 找到当前用户的库存
-			query = new HashMap<>();
-			query.put("userId",user.getUserId());
-			query.put("status","0");
-			query.put("type",order.getGoodsId());
-			List<GmGoodsUserDO> goodsUserList =goodsUserService.list(query);
+			for (int j = 0; j < orderList.size(); j++) {
+				// 获取订单
+				GmOrderDO order = orderList.get(j);
+				if(order.getOrderStatus()!=1){
+					return R.error("已处理或失效订单");
+				}
+				// 通过当前账户id 和 订单的商品id 找到当前用户的库存
+				query = new HashMap<>();
+				query.put("userId",user.getUserId());
+				query.put("status","0");
+				query.put("type",order.getGoodsId());
+				List<GmGoodsUserDO> goodsUserList =goodsUserService.list(query);
 
-			// 解析传来的二维码
-			String[]goodsCodes = ids.split(",");
-			Integer size = goodsCodes.length;
-			if(size>goodsUserList.size()){
-				return R.error("上级库存不足");
-			}
+				// 解析传来的二维码
+				String[]goodsCodes = ids.split(",");
+				Integer size = goodsCodes.length;
+				if(size>goodsUserList.size()){
+					return R.error("上级库存不足");
+				}
 
-			// 根据二维码的数量 遍历库存 给库存添加二维码
-			broken:for (int i = 0; i < size; i++) {
+				// 根据二维码的数量 遍历库存 给库存添加二维码
+				broken:for (int i = 0; i < size; i++) {
 
-				// 如果是平台管理者 则跟新平台库存
-				if(isAdmin){
-					GmGoodsInfoDO goodsInfoDO = goodsInfoService.get(order.getGoodsId());
-					if(goodsInfoDO!=null){
-						Integer remark = Integer.parseInt(goodsInfoDO.getRemark());
-						if(remark>0){
-							remark -= 1;
-							goodsInfoDO .setRemark(remark+"");
-							goodsInfoService.update(goodsInfoDO);
-						}else {
-							goodsInfoDO.setStatu(2);
-							goodsInfoService.update(goodsInfoDO);
-							isHave = false;
-							break broken;
+					// 如果是平台管理者 则跟新平台库存
+					if(isAdmin){
+						GmGoodsInfoDO goodsInfoDO = goodsInfoService.get(order.getGoodsId());
+						if(goodsInfoDO!=null){
+							Integer remark = Integer.parseInt(goodsInfoDO.getRemark());
+							if(remark>0){
+								remark -= 1;
+								goodsInfoDO .setRemark(remark+"");
+								goodsInfoService.update(goodsInfoDO);
+							}else {
+								goodsInfoDO.setStatu(2);
+								goodsInfoService.update(goodsInfoDO);
+								isHave = false;
+								break broken;
+							}
+						}
+
+
+					}
+
+					String goodsCode = goodsCodes[i];
+					Boolean flag = true;
+					//先根据二维码查询
+					Map<String,Object> queryGoodsUser = new HashMap<>();
+					queryGoodsUser.put("goodsCode",goodsCode);
+					queryGoodsUser.put("userId",user.getUserId());
+					queryGoodsUser.put("status","0");
+					List<GmGoodsUserDO> goodsUserDOList = goodsUserService.list(queryGoodsUser);
+					GmGoodsUserDO goodsUserDO = null;
+
+					if(goodsUserDOList!=null&&goodsUserDOList.size()>0){
+						goodsUserDO = goodsUserDOList.get(0);
+						// 先将该库存 改变状态为已发
+						goodsUserDO.setStatus("1");
+						goodsUserDO.setOutTime(DateUtil.getDateTime());
+						if(goodsUserService.update(goodsUserDO)<0){
+							flag = false;
+						}
+						// 然后给 下级 新建一个库存
+						goodsUserDO.setUserId(order.getUserId());
+						goodsUserDO.setId(null);
+						goodsUserDO.setStatus("0");
+						goodsUserDO.setInTime(DateUtil.getDateTime());
+						goodsUserDO.setOutTime(null);
+						if(goodsUserService.save(goodsUserDO)<0){
+							flag = false;
+						}
+					}else { // 如果查不到二维码 则下发新的
+						Boolean isNoCode = false;
+						loop:for (GmGoodsUserDO temp:goodsUserList) {
+							if(temp.getGoodsCode()==null||temp.getGoodsCode().equals("")){
+								isNoCode = true;
+								goodsUserDO = temp;
+								break loop;
+							}
+						}
+						if(!isNoCode){
+							return R.error("二维码错误");
+						}
+						// 先将该库存设置一个二维码 并改变状态为已发
+						goodsUserDO.setStatus("1");
+						goodsUserDO.setGoodsCode(goodsCode);
+						goodsUserDO.setOutTime(DateUtil.getDateTime());
+						if(goodsUserService.update(goodsUserDO)<0){
+							flag = false;
+						}
+						// 然后给 下级 新建一个库存 并设置二维码
+						goodsUserDO.setUserId(order.getUserId());
+						goodsUserDO.setId(null);
+						goodsUserDO.setStatus("0");
+						goodsUserDO.setInTime(DateUtil.getDateTime());
+						goodsUserDO.setOutTime(null);
+						if(goodsUserService.save(goodsUserDO)<0){
+							flag = false;
+						}
+					}
+
+
+					// 分润开始 fuck me...
+					if(flag){
+						if(type==2&&userProfit!=null&&parentProfit!=null){ //联合创始人
+							GmProfitDetailDO profitDetail = new GmProfitDetailDO();
+							// 给下级发货 自身直接奖励80
+							profitDetail.setAmount(REWARD_A);
+							profitDetail.setCreateTime(DateUtil.getDateTime());
+							profitDetail.setStatus(2);
+							// 谁奖励的 这里平台奖励的 也就是admin
+							profitDetail.setParentId(parent.getUserId());
+							profitDetail.setProfitId(userProfit.getId());
+							profitDetail.setRemark(order.getType());
+							profitDetailService.save(profitDetail);
+							//  推荐人奖励 50
+							profitDetail.setAmount(REWARD_B);
+							profitDetail.setProfitId(parent.getUserId());
+							profitDetailService.save(profitDetail);
+						}else if(type==3&&userProfit!=null){ //若果是经销商 发给下级或 直接给自己分润
+							// 给下级发货 自身直接奖励80
+							GmProfitDetailDO profitDetail = new GmProfitDetailDO();
+							profitDetail.setAmount(REWARD_A);
+							profitDetail.setCreateTime(DateUtil.getDateTime());
+							profitDetail.setStatus(2);
+							profitDetail.setParentId(parent.getUserId());
+							profitDetail.setProfitId(userProfit.getId());
+							profitDetail.setRemark(order.getType());
+							profitDetailService.save(profitDetail);
 						}
 					}
 
 
 				}
 
-				String goodsCode = goodsCodes[i];
-				Boolean flag = true;
-				//先根据二维码查询
-				Map<String,Object> queryGoodsUser = new HashMap<>();
-				queryGoodsUser.put("goodsCode",goodsCode);
-				queryGoodsUser.put("userId",user.getUserId());
-				queryGoodsUser.put("status","0");
-				List<GmGoodsUserDO> goodsUserDOList = goodsUserService.list(queryGoodsUser);
-				GmGoodsUserDO goodsUserDO = null;
-
-				if(goodsUserDOList!=null&&goodsUserDOList.size()>0){
-					goodsUserDO = goodsUserDOList.get(0);
-					// 先将该库存 改变状态为已发
-					goodsUserDO.setStatus("1");
-					goodsUserDO.setOutTime(DateUtil.getDateTime());
-					if(goodsUserService.update(goodsUserDO)<0){
-						flag = false;
-					}
-					// 然后给 下级 新建一个库存
-					goodsUserDO.setUserId(order.getUserId());
-					goodsUserDO.setId(null);
-					goodsUserDO.setStatus("0");
-					goodsUserDO.setInTime(DateUtil.getDateTime());
-					goodsUserDO.setOutTime(null);
-					if(goodsUserService.save(goodsUserDO)<0){
-						flag = false;
-					}
-				}else { // 如果查不到二维码 则下发新的
-					Boolean isNoCode = false;
-					loop:for (GmGoodsUserDO temp:goodsUserList) {
-						if(temp.getGoodsCode()==null||temp.getGoodsCode().equals("")){
-							isNoCode = true;
-							goodsUserDO = temp;
-							break loop;
-						}
-					}
-					if(!isNoCode){
-						return R.error("二维码错误");
-					}
-					// 先将该库存设置一个二维码 并改变状态为已发
-					goodsUserDO.setStatus("1");
-					goodsUserDO.setGoodsCode(goodsCode);
-					goodsUserDO.setOutTime(DateUtil.getDateTime());
-					if(goodsUserService.update(goodsUserDO)<0){
-						flag = false;
-					}
-					// 然后给 下级 新建一个库存 并设置二维码
-					goodsUserDO.setUserId(order.getUserId());
-					goodsUserDO.setId(null);
-					goodsUserDO.setStatus("0");
-					goodsUserDO.setInTime(DateUtil.getDateTime());
-					goodsUserDO.setOutTime(null);
-					if(goodsUserService.save(goodsUserDO)<0){
-						flag = false;
-					}
+				if(!isHave){
+					return R.error("库存不足,请前往平台上货");
 				}
-
-
-				// 分润开始 fuck me...
-				if(flag){
-					if(type==2&&userProfit!=null&&parentProfit!=null){ //联合创始人
-						GmProfitDetailDO profitDetail = new GmProfitDetailDO();
-						// 给下级发货 自身直接奖励80
-						profitDetail.setAmount(REWARD_A);
-						profitDetail.setCreateTime(DateUtil.getDateTime());
-						profitDetail.setStatus(2);
-						// 谁奖励的 这里平台奖励的 也就是admin
-						profitDetail.setParentId(parent.getUserId());
-						profitDetail.setProfitId(userProfit.getId());
-						profitDetail.setRemark(order.getType());
-						profitDetailService.save(profitDetail);
-						//  推荐人奖励 50
-						profitDetail.setAmount(REWARD_B);
-						profitDetail.setProfitId(parent.getUserId());
-						profitDetailService.save(profitDetail);
-					}else if(type==3&&userProfit!=null){ //若果是经销商 发给下级或 直接给自己分润
-						// 给下级发货 自身直接奖励80
-						GmProfitDetailDO profitDetail = new GmProfitDetailDO();
-						profitDetail.setAmount(REWARD_A);
-						profitDetail.setCreateTime(DateUtil.getDateTime());
-						profitDetail.setStatus(2);
-						profitDetail.setParentId(parent.getUserId());
-						profitDetail.setProfitId(userProfit.getId());
-						profitDetail.setRemark(order.getType());
-						profitDetailService.save(profitDetail);
-					}
-				}
-
-
+				order.setOrderStatus(2);
+				order.setEndTime(DateUtil.getDateTime());
+				gmOrderService.update(order);
 			}
-
-			if(!isHave){
-				return R.error("库存不足,请前往平台上货");
-			}
-			order.setOrderStatus(2);
-			order.setEndTime(DateUtil.getDateTime());
-			gmOrderService.update(order);
 			return R.ok();
 		}catch (Exception e){
 			e.printStackTrace();
